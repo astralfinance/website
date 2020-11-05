1c63a3af5f78769a37bc9bb46474a29d1eff9650
dose <>
2020-11-05 08:32:40 -0500

    under construction

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Back from './assets/back.jpg';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+  <img src={Back} />
+NEW WEBSITE COMING SOON
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
